5f66024bbc931ee5ddef83a51d4f9f5076a5ea6a
Bearzilasaur <mitchell.baum@gmail.com>
2022-03-29 12:51:14 +1100

    Added select multiple images, added direct pdf generation

--- imageCaptionerClass.py
+++ imageCaptionerClass.py
@@ -5,6 +5,8 @@ from PIL import ImageTk, Image
 from os import mkdir, path, listdir
 from typing import List, Tuple
 
+from LaTeXBuilder import LaTeXBuilder
+
 
 class ImageCaptioner(tk.Tk):
     def __init__(self, imagemax=(500, 500)):
@@ -49,6 +51,17 @@ class ImageCaptioner(tk.Tk):
         )
         filebutt.place(relx=0.1, rely=0.925)
 
+        filebutt = tk.Button(
+            self.canvas,
+            text="Select Individual Images",
+            padx=10,
+            pady=5,
+            fg="black",
+            command=self.get_paths,
+        )
+        filebutt.place(relx=0.22, rely=0.925)
+
+
         # add next button
         self.nextbutt = tk.Button(
             self.iframe,
@@ -75,20 +88,47 @@ class ImageCaptioner(tk.Tk):
         )
         self.save.place(relx=0.8, rely=0.925)
 
+                # save files locally
+        self.tex = tk.Button(
+            self.canvas,
+            text="Generate pdf",
+            padx=10,
+            pady=5,
+            fg="black",
+            command=self._buildLaTeX,
+        )
+        self.tex.place(relx=0.9, rely=0.925)
     # ================ FUNCTIONS =================#
 
-    # get images from directory (user selects directory)
+    # get images from directory (user selects directory) DEPRECATED
     def getDir(self):
-        dirpath = filedialog.askdirectory(initialdir="./", title="Select Images Folder")
+        dirpath = filedialog.askdirectory(initialdir="./",
+                                                            title="Select Images Folder")
         images = [
-            [path.join(dirpath, file), file]
+            [path.abspath(path.join(dirpath, file))]
             for file in listdir(dirpath)
             if file.endswith((".jpeg", ".jpg"))
         ]
+
         self.imageIncrementer = 0
         self.numberImages = len(images)
         self.imagepaths = images
-        self.workingImage = self.imagepaths[self.imageIncrementer][1]
+        self.workingImage = self.imagepaths[self.imageIncrementer][0],
+        self.listImages(self.imagepaths)
+        self.load_image(self.imagepaths[self.imageIncrementer][0], self.imagemax)
+
+    # user select individual photos
+    def get_paths(self):
+        paths = filedialog.askopenfilenames(initialdir='./',
+                                                                   title='Select individual images to caption...',
+                                                                   filetypes = (("JPEG, JPG", "*.jp*"), ("All Files", "*.*")))
+        paths = [[path.abspath(file)] for file in paths]
+        print(f"paths: {paths}\n\n")
+        self.imageIncrementer=0
+        self.workingdir = path.dirname(paths[self.imageIncrementer][0])
+        self.numberImages = len(paths)
+        self.imagepaths = paths
+        self.workingImage = self.imagepaths[self.imageIncrementer][0],
         self.listImages(self.imagepaths)
         self.load_image(self.imagepaths[self.imageIncrementer][0], self.imagemax)
 
@@ -97,13 +137,14 @@ class ImageCaptioner(tk.Tk):
         for item in self.dirframe.winfo_children():
             item.destroy()
         for image in imagelist:
-            if image[1] == self.workingImage:
+            if image[0] == self.workingImage:
                 colour = "yellow"
             else:
                 colour = "grey"
-            label = tk.Label(self.dirframe, text=image[1], bg=colour)
+            label = tk.Label(self.dirframe, text=image, bg=colour)
             label.pack()
 
+    # loads the image into the window
     def load_image(self, imagepath: str, imagemax: Tuple):
         with Image.open(imagepath) as img:
             if img.mode in ("RGBA", "P"):
@@ -120,7 +161,7 @@ class ImageCaptioner(tk.Tk):
         for label in self.imgframe.winfo_children():
             label.destroy()
 
-        # load the thumbnail
+         # load the thumbnail
         img = ImageTk.PhotoImage(Image.open(temppath))
         self.image = tk.Label(self.imgframe, image=img)
         self.image.image = img
@@ -131,28 +172,33 @@ class ImageCaptioner(tk.Tk):
         for i in self.imagepaths:
             print(f"\n\n{i}\n\n")
 
+
     # get the self.caption for the image
     def bake_caption(self):
-        self.captiontext = (
-            self.caption.get()
-        )  # "1.0","end-1c") # these options make the returned string nicer
-        # do work here!
+        self.captiontext = (self.caption.get())  # "1.0","end-1c") # these options make the returned string nicer
         self.imagepaths[self.imageIncrementer].append(self.captiontext)
         self.caption.delete(0, "end")  # clears the input widget
 
         # increment the image counter and then do the next image
-        if self.imageIncrementer >= self.numberImages:
+        if self.imageIncrementer >= self.numberImages-1:
+            
+            # delete old image
+            for label in self.imgframe.winfo_children():
+                label.destroy()
+            
             print("END OF FILES")
-            # checks
+            # # checks
             print(f"\n\n\n\nself.imagepaths: {self.imagepaths}\n\n\n\n")
             for i in self.imagepaths:
                 print(f"\n\n{i}\n\n")
+    
         else:
             self.imageIncrementer += 1
-        self.workingImage = self.imagepaths[self.imageIncrementer][1]
-        self.listImages(self.imagepaths)
-        self.load_image(self.imagepaths[self.imageIncrementer][0], self.imagemax)
+            self.workingImage = self.imagepaths[self.imageIncrementer][0],
+            self.listImages(self.imagepaths)
+            self.load_image(self.imagepaths[self.imageIncrementer][0], self.imagemax)
 
+    # exports the self.imagepaths to csv
     def export_captions(self):
         filename = filedialog.asksaveasfilename(
             initialdir="./",
@@ -164,8 +210,14 @@ class ImageCaptioner(tk.Tk):
         with open(filename, "w") as outfile:
             writer = csv.writer(outfile)
             for row in self.imagepaths:
-                writer.writerow(row)
+                if row: # removes empty rows
+                    writer.writerow(row)
 
+    # calls the LaTeXBuilder class to build latex file directly without using LaTexBuilder gui
+    def _buildLaTeX(self):
+        tex = LaTeXBuilder()
+        tex.construct(self.imagepaths)
+        tex.generate(initdir=self.workingdir, useroutput=True)
 
 if __name__ == "__main__":
     app = ImageCaptioner()
